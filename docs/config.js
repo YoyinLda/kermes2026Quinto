7949210c56b5d9103b7b4666ec47b25ebe7df155
Rodrigo Sanchez <rodrigo.sanchez@autofact.cl>
2026-09-03 14:51:03 -0400

    add config

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,3 @@
 // Pega el enlace de la implementación de Apps Script, terminado en /exec.
 // Este enlace es público; el código ACCESS_CODE nunca debe ir aquí.
-window.KERMES_APP_URL = '';
+window.KERMES_APP_URL = 'https://script.google.com/macros/s/AKfycbx0jmljBu7WTKqXYBqjXc308MdmAo_AngDgzfQ8d9ONFm3JTRkMB4StwJLUaSzWh3buGg/exec';
